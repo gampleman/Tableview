7241db4616391903fdd9d006b16d41bb2759b002
Jakub Hampl <honitom@seznam.cz>
2011-06-01 02:32:56 +0100

    Debugging heroku

--- lib/tableview/helper.rb
+++ lib/tableview/helper.rb
@@ -10,7 +10,7 @@ module Tableview::Helper
     end
     
     def method_missing(name, *args, &blk)
-      puts m.inspect, name.inspect
+      puts name.inspect
       if m = name.to_s.match(/^(.+)\?$/)
         puts m.inspect, name.inspect
         @params[:format] == m.captures.first
