b5cfac111e8224838b52eaf4f92f84df2a5fbccd
Jakub Hampl <honitom@seznam.cz>
2011-06-01 02:44:18 +0100

    Debugging heroku

--- lib/tableview/helper.rb
+++ lib/tableview/helper.rb
@@ -12,9 +12,11 @@ module Tableview::Helper
     def method_missing(name, *args, &blk)
       puts name.inspect
       if m = name.to_s.match(/^(.+)\?$/)
-        puts m.inspect, name.inspect
+        puts "matched"
+        #puts m.inspect, name.inspect
         @params[:format] == m.captures.first
       else
+        puts "did not match"
         super
       end
     end
