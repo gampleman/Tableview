7d6e66668e2847f886e17ea92b0f5e3b65bbb564
Jakub Hampl <honitom@seznam.cz>
2011-06-01 01:51:27 +0100

    Renamed partial generator.

---
+++ lib/generators/tableview/tableview_generator.rb
@@ -0,0 +1,22 @@
+require 'rails'
+
+module Tableview
+  module Generators
+    class TableviewGenerator < ::Rails::Generators::NamedBase
+      
+      def create_partial
+        cols = file_name.camelize.constantize.column_names
+        template = "=> @#{file_name.tableize}\n\n"
+        cols.each do |col|
+          template << "+ :#{col}\n"
+        end
+        create_file "app/views/#{file_name.tableize}/_table.tv", template
+      end
+      
+      def install_into_controller
+        inject_into_class "app/controllers/#{file_name.tableize}_controller.rb", file_name.camelize.pluralize + "Controller", "  self.responder = ApplicationResponder\n  respond_to :html, :json, :csv, :xls, :ascii"
+      end
+      
+    end
+  end
+end
